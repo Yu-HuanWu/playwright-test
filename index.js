bda8d66f0eced7f1d8e4fae67c3a0d963d45ae82
Yu-HuanWu <yuhuanhwu@gmail.com>
2021-11-17 23:30:36 -0800

    testing on ewetube

--- index.js
+++ index.js
@@ -11,18 +11,19 @@ const { chromium } = require("playwright");
 
   const page = await context.newPage();
 
-  await page.goto("https://www.netflix.com");
-  await page.click(".authLinks.redButton");
-  await page.type('.hybrid-login-form .nfEmailPhoneControls .nfTextField', 'an_invalid_wolf_email');
-  await page.type('.hybrid-login-form .nfPasswordControls .nfTextField', 'password')
-  await page.keyboard.press('Enter', {delay: 2000});
+  await page.goto("https://ewetube.herokuapp.com/#/");
+  await page.click('.login');
+  await page.type('css=[placeholder="Username"]', 'random_invalid_url');
+  await page.type('css=[placeholder="Password"]', 'password');
+  // await page.keyboard.press('Enter', {delay: 2000});
+  await page.click('.form-box button');
   try {
-    await page.waitForSelector('.ui-message-contents', { timeout: 5000 });
-    const content = await page.textContent('.ui-message-contents');
+    await page.waitForSelector('.error', { timeout: 5000 });
+    const content = await page.textContent('.error');
     console.log(content);
-    console.log("Test successful: cannot sign in with an invalid email/password combination")
+    console.log("Test successful: cannot sign in with an invalid username/password combination")
   } catch {
-    console.log('Test failed or the email/password combination was unintentionally valid');
+    console.log('Test failed or the username/password combination was unintentionally valid');
   }
     // YOUR CODE ENDS
 })();
